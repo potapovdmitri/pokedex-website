ad18220546635c3e74fda032fa66aaaaf2a0194e
potapovdmitri <potapovdrpotapov@gmail.com>
2024-01-28 00:57:57 +0200

    Add or update files in public folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import { useState } from 'react';
 import './App.css';
 import Header from './components/UI/Header';
-import PokemonList from './components/UI/content/PokemonList';
 import Main from './components/UI/Main';
 
 function App() {
